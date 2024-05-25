3295b41597bb50812a8fca0c58fde48594a47c34
jiahui <2303058491@qq.com>
2024-05-25 20:47:12 +0800

    Initial Commit

---
+++ cvios/cviosApp.swift
@@ -0,0 +1,17 @@
+//
+//  cviosApp.swift
+//  cvios
+//
+//  Created by fujiahui on 2024/5/25.
+//
+
+import SwiftUI
+
+@main
+struct cviosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
